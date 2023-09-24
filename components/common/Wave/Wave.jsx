c6c73bfbb54dec8f803ea180769ad632130d63b6
Shanu Raj <shanuraj715@gmail.com>
2023-09-24 11:43:16 +0530

    added wave effect to homepage

---
+++ components/common/Wave/Wave.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './styles.module.scss'
+import MySvg from './Svg'
+
+function Wave() {
+
+
+    
+
+
+  return <div className={styles.svgContainer}>
+    <MySvg />
+  </div>
+}
+
+export default Wave
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import seoData from '@/utils/seoData'
 import { NextSeo } from 'next-seo'
+import Wave from '@/components/common/Wave/Wave'
 
 function Home() {
   return (
     <>
       <NextSeo {...seoData.homepage} />
       <div className="">
-        Homepage
+        <Wave />
       </div>
     </>
   )
