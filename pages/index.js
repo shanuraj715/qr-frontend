c08918c0408f212c81e79370929049825a6ed9ab
ShanuRaj715 <shanuraj715@gmail.com>
2024-01-07 00:50:44 +0530

    Reverted homepage

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import seoData from '@/utils/seoData'
 import { NextSeo } from 'next-seo'
 import Wave from '@/components/common/Wave/Wave'
 import Carousel from '@/components/common/Carousel/Carousel'
-import toast from 'react-hot-toast'
 
 function Home() {
   return (
