d2081822c29cfc486bbf1b292f21860b24a205a0
ShanuRaj715 <shanuraj715@gmail.com>
2024-01-07 00:42:53 +0530

    Increase toast message time

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,17 @@ import '@/styles/globals.scss'
 import { Roboto_Slab } from 'next/font/google'
 import Layout from '@/Layouts/Layout'
 import { Tooltip } from 'react-tooltip';
-import { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 
 const roboto_slab = Roboto_Slab({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
+
   return <div className={`${roboto_slab.className}`}>
     <Layout>
       <Component {...pageProps} />
     </Layout>
     <Tooltip id="my-tooltip-data-html" />
-    <Toaster position="bottom-left" reverseOrder={false} />
+    <Toaster position="bottom-left" reverseOrder={false} toastOptions={{duration: 5000}} />
   </div>
 }
