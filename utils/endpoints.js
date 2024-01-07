3d6b90ae5efd21b5c588a3f1411b2034296348cc
ShanuRaj715 <shanuraj715@gmail.com>
2024-01-07 11:27:29 +0530

    added resne OTP for registration

--- utils/endpoints.js
+++ utils/endpoints.js
@@ -3,3 +3,4 @@ import { API_URL } from "@/constants"
 export const USER_LOGIN = `${API_URL}/user/login`
 export const USER_REGISTER = `${API_URL}/user/register`
 export const USER_ACCOUNT_OTP_VERIFY = `${API_URL}/user/verifyOTP`
+export const USER_ACCOUNT_OTP_RESEND = `${API_URL}/user/resendOTP`
